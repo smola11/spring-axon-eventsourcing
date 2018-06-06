125c05b7ea482f1a751b0cd3c12fd42ca0f76317
Smolarz <mjsz@gft.com>
2018-06-06 13:30:33 +0200

    Skeleton of the app

---
+++ src/main/java/com/maciej/springaxoneventsourcing/SpringAxonEventsourcingApplication.java
@@ -0,0 +1,12 @@
+package com.maciej.springaxoneventsourcing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAxonEventsourcingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAxonEventsourcingApplication.class, args);
+	}
+}
