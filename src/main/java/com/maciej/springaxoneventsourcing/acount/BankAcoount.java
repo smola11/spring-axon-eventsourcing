2e93f3784f00e20677928e5fadcb62b514bc1fed
Smolarz <mjsz@gft.com>
2018-06-06 14:05:14 +0200

    Added AccountApi handling REST requests

--- src/main/java/com/maciej/springaxoneventsourcing/acount/BankAcoount.java
+++ src/main/java/com/maciej/springaxoneventsourcing/acount/BankAcoount.java
@@ -1,9 +1,11 @@
 package com.maciej.springaxoneventsourcing.acount;
 
 import org.axonframework.commandhandling.model.AggregateIdentifier;
+import org.axonframework.spring.stereotype.Aggregate;
 
 import java.io.Serializable;
 
+@Aggregate
 public class BankAcoount implements Serializable {
 
     private static final long serialVersionUID = 1L;
